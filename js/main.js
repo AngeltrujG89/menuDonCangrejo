70a69aa5b492b0a7956a1d8d633900775cf1c17e
AngeltrujG89 <latg8904@hotmail.com>
2024-03-10 21:30:47 -0600

    deteccion de click o touch en partes laterales para cambiar de pagina

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   let currentPage = 1;
+  let isTransitioning = false; // Variable para controlar la transición de página
   const pages = document.querySelectorAll('.page');
 
   function showPage(pageNumber) {
@@ -8,16 +9,24 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function nextPage() {
-    if (currentPage < pages.length) {
+    if (!isTransitioning && currentPage < pages.length) {
+      isTransitioning = true; // Bloquear transición
       currentPage++;
       showPage(currentPage);
+      setTimeout(() => {
+        isTransitioning = false; // Desbloquear transición después de un tiempo
+      }, 500); // Tiempo de bloqueo en milisegundos
     }
   }
 
   function previousPage() {
-    if (currentPage > 1) {
+    if (!isTransitioning && currentPage > 1) {
+      isTransitioning = true; // Bloquear transición
       currentPage--;
       showPage(currentPage);
+      setTimeout(() => {
+        isTransitioning = false; // Desbloquear transición después de un tiempo
+      }, 500); // Tiempo de bloqueo en milisegundos
     }
   }
 
@@ -25,10 +34,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const screenWidth = window.innerWidth;
     const touchX = event.type === "touchstart" ? event.touches[0].clientX : event.clientX;
 
-    if (touchX > screenWidth / 2) {
-      nextPage();
-    } else {
-      previousPage();
+    // Definir un umbral para determinar si el toque o clic está en el lado izquierdo o derecho
+    const threshold = screenWidth * 0.3; // Por ejemplo, un umbral del 30%
+
+    if (touchX < threshold) {
+      previousPage(); // Retroceder si el toque o clic está en el lado izquierdo
+    } else if (touchX > screenWidth - threshold) {
+      nextPage(); // Avanzar si el toque o clic está en el lado derecho
     }
   }
 
